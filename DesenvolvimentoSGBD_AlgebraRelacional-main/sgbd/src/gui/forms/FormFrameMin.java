9a412c0a6dfd8b8a55f47477ec0766c8bb6b15b1
luanaFOliveira <luluoliveira7@gmail.com>
2023-02-28 20:40:06 -0300

    new changes

---
+++ DesenvolvimentoSGBD_AlgebraRelacional-main/sgbd/src/gui/forms/FormFrameMin.java
@@ -0,0 +1,143 @@
+package gui.forms;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import com.mxgraph.model.mxCell;
+
+import entities.Cell;
+import entities.OperatorCell;
+import sgbd.query.Operator;
+import sgbd.query.unaryop.FilterColumnsOperator;
+import sgbd.query.unaryop.MaxOperator;
+import sgbd.query.unaryop.MinOperator;
+import sgbd.table.Table;
+import util.TableFormat;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("serial")
+public class FormFrameMin extends JFrame implements ActionListener{
+
+	private JPanel contentPane;
+	private JButton btnPronto;
+	private JComboBox comboBox;
+	private List<String> columnsList;
+	private Cell cell;
+	private Cell parentCell;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(Object cell, List<Cell> cells) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FormFrameMax frame = new FormFrameMax(cell,cells);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FormFrameMin(Object cell, List<Cell> cells) {
+		this.setVisible(true);
+		
+		this.cell = cells.stream().filter(x -> x.getCell().equals(((mxCell)cell))).findFirst().orElse(null);
+		parentCell = this.cell.getParents().get(0);
+		
+		initializeGUI();
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void initializeGUI() {
+		this.setVisible(true);
+		setBounds(100, 100, 450, 183);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		columnsList = new ArrayList<String>();
+		
+		columnsList = parentCell.getColumnsName();
+		
+		comboBox = new JComboBox(columnsList.toArray(new String[0]));
+		
+				
+		btnPronto = new JButton("Pronto");
+		btnPronto.addActionListener(this);
+
+		
+		JLabel lblNewLabel = new JLabel("Colunas");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(51)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 155, GroupLayout.PREFERRED_SIZE)
+							.addGap(30)
+							.addComponent(btnPronto)))
+					.addContainerGap(123, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(23)
+					.addComponent(lblNewLabel)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnPronto))
+					.addContainerGap(186, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == btnPronto) {
+			executeOperation(comboBox.getSelectedItem().toString());
+			
+		}
+		
+		
+	}
+	
+
+	public void executeOperation(String column) {
+		
+		
+		Operator operator = new MinOperator(parentCell.getData(),parentCell.getSourceTableName(column),column);
+	    operator.open();
+	    
+	    ((OperatorCell) cell).setOperator(operator, TableFormat.getRows(operator));
+	    
+        operator.close();
+        
+        dispose();
+		
+	}
+}
